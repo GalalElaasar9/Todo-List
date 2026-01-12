eaedbc3172934f77c0fc3c1127e2ae816e028c85
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2026-01-12 04:51:59 +0200

    .

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -15,14 +15,25 @@ import {v4 as uuidv4} from 'uuid';
 import toast from 'react-hot-toast';
 // import { FaFilePen } from "react-icons";
 
+import {
+  // Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  DialogContentText 
+  // TextField,
+} from "@mui/material";
 
 export default function TodoList() {
   let {todos , setTodos} = useContext(TodosContext);
 
   const [titleInput , setTitleInput] = useState('');
   const [detailsInput , setDetailsInput] = useState('');
-
   const [displayTodosType , setDisplayTodosType] = useState('all');
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [dialogTodo, setDialogTodo] = useState(null);
+  const [showUpdateDialog, setShowUpdateDialog] = useState(false);
 
   // Filteration Arrays
   /*
@@ -58,11 +69,6 @@ export default function TodoList() {
   }else{
     todosToBeRenderd = todos;
   }
-
-  const todosJsx = todosToBeRenderd.map((t)=>{
-    return <Todo key={t.id} todo={t}/>
-  })
-
 
   useEffect(()=>{
     const storageTodos = JSON.parse(localStorage.getItem("todos"));
@@ -137,105 +143,249 @@ export default function TodoList() {
     }
   }
 
+  // ================== Start Functions Delete Dialog ==================
+  function openDeleteDialog(todo) {
+    // todo => prop دى وراحت عن طريق fun الى هى شايلة ال showDelete عن طريق ال todo لأنة راجع على هيئة أوبجيكت صفحة ال todo بتاع ال id عملتله باص علشان يجيب ال param عبارة عن 
+    setDialogTodo(todo) // state علشان نقدر تغيرها بعد كدا لأنها أصبحت setDialogTodo بالكامل لل todo هنا باصينا قيمة ال
+    // alert(todo.id)
+    setShowDeleteDialog(true)
+  }
+
+  const handleDeleteDialogClose = () => {
+    setShowDeleteDialog(false);
+  };
+
+  // Function handleDeleteConfirm
+  function handleDeleteConfirm() {
+    // console.log(dialogTodo);
+    const TodosAfterDelete = todos.filter((t) => t.id !== dialogTodo.id);
+    setTodos(TodosAfterDelete);
+    // علشان لما أعمل ريفريش يتعرض أخر حذف delete بعد ال todos هنا بحفظ ال
+    localStorage.setItem("todos",JSON.stringify(TodosAfterDelete))
+
+    setShowDeleteDialog(false)
+    toast.success("تم حذف المهمة بنجاح",{
+      duration:2000,
+      position:"top-right"
+    })
+  }
+
+  // ================== End Functions Delete Dialog ==================
+
+    // ================== Start Functions Update Dialog ==================
+  const openUpdateDialog = (todo) => {
+    setDialogTodo(todo)
+    setShowUpdateDialog(true)
+  };
+  const handleUpdateDialogClose = () => {
+    setShowUpdateDialog(false);
+  };
+
+  // Function handleUpdateConfirm
+  function handleUpdateConfirm() {
+    const todosAfterEdit = todos.map((t)=>{
+      if(t.id === dialogTodo.id){
+        return {...t , title: dialogTodo.title , details:dialogTodo.details}
+      }else{
+        return t;
+      }
+    })
+    toast.success("تم تعديل المهمة بنجاح",{
+      duration:2000,
+      position:"top-right"
+    })
+    setTodos(todosAfterEdit);
+    setShowUpdateDialog(false);
+    // علشان لما أعمل ريفريش يتعرض أخر تعديل update بعد ال todos هنا بحفظ ال
+    localStorage.setItem("todos",JSON.stringify(todosAfterEdit))
+  }
+
+  // ================== End Functions Update Dialog ==================
+
+  const todosJsx = todosToBeRenderd.map((t)=>{
+    return <Todo key={t.id} todo={t} showDelete={openDeleteDialog} showUpdate={openUpdateDialog}/>
+  })
+
   return (
-    <Container maxWidth="md">
-      {todos.length === 0 ? <Card sx={{ minWidth: 275 , maxHeight:"650px" , height:"100%" , overflow:"auto"  }}>
-        <CardContent>
-          <Typography variant='h2' className='title text-center !mb-4' style={{ fontWeight:"400" }}>
-            مهامى اليومية 
-          </Typography>
-          <Divider/>
-          {/* Filter Buttons */}
-          <ToggleButtonGroup
-            exclusive          
-            value={displayTodosType}  
-            aria-label="text alignment"
-            className="!flex !justify-center items-center ltr my-5"
-            onChange={changeDisplayedType}
-            color='primary'
+    <>
+      {/* DELETE DIALOG */}
+      <Dialog
+        open={showDeleteDialog}
+        onClose={handleDeleteDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          هل أنت متأكد من حذف هذة المهمة ؟
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            لا يمكن التراجع عن الحذف فى حال إختيار زر (حذف)
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions className="!justify-start">
+          <Button
+            onClick={handleDeleteConfirm}
+            className="!text-[#b23c17]"
+            autoFocus
           >
-            <ToggleButton value="non-completed">
-              غير المنجز
-            </ToggleButton>
-            <ToggleButton value="completed">
-              المنجز
-            </ToggleButton>
-            <ToggleButton value="all">
-              الكل 
-            </ToggleButton>
-          </ToggleButtonGroup>
-          {/* Filter Buttons */}
-          {/* Alert No assignments */}
-            <Alert severity="error" className='mb-3 justify-center flex-row-reverse gap-2'>لا يوجد مهام مضافة حتى الآن</Alert>
-          {/* Alert No assignments */}
-          {/* Start Input + Add Button */}
-          <div className="inputsFeilds">
-            <Grid container spacing={2} direction={'row-reverse'} >
-              <Grid size={4}>
-                <TextField value={titleInput} onChange={(e)=>(setTitleInput(e.target.value))} id="outlined-basic" label="عنوان المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField} />
-              </Grid>
-              <Grid size={4}>
-                <TextField value={detailsInput} onChange={(e)=>(setDetailsInput(e.target.value))} id="outlined-basic" label="تفاصيل المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField} />
-              </Grid>
-              <Grid size={4}>
-                <Button onClick={()=>{handleAddClick()}} variant="contained" sx={{ width:"100%" , height:"100%" }} className={`${titleInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${detailsInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'}`}>إضافة</Button>
-              </Grid>
-            </Grid>
-          </div>
-          {/* End Input + Add Button */}
-        </CardContent>
-      </Card> : <Card sx={{ minWidth: 275 , maxHeight:"650px" , height:"100%" , overflow:"auto"  }}>
-        <CardContent>
-          <Typography variant='h2' className='text-center !mb-4' style={{ fontWeight:"400" }}>
-            مهامى اليومية
-          </Typography>
-          <Divider/>
-          {/* Filter Buttons */}
-          <ToggleButtonGroup
-            exclusive          
-            value={displayTodosType}  
-            aria-label="text alignment"
-            className="justify-center items-center ltr my-5"
-            onChange={changeDisplayedType}
-            color='primary'
+            نعم قم بالحذف
+          </Button>
+          <Button onClick={handleDeleteDialogClose} className="!text-[#b23c17]">
+            إغلاق
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* DELETE DIALOG */}
+            {/* UPDATE DIALOG */}
+      <Dialog
+        open={showUpdateDialog}
+        onClose={handleUpdateDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">تعديل المهمة</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            label="عنوان المهمة"
+            fullWidth
+            variant="standard"
+            value={dialogTodo?.title}
+            onChange={(e) => {
+              setDialogTodo({ ...dialogTodo, title: e.target.value });
+            }}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            label="التفاصيل"
+            fullWidth
+            variant="standard"
+            value={dialogTodo?.details}
+            onChange={(e) => {
+              setDialogTodo({ ...dialogTodo, details: e.target.value });
+            }}
+          />
+        </DialogContent>
+        <DialogActions className="!justify-start">
+          <Button
+            onClick={handleUpdateConfirm}
+            className="!text-[#b23c17]"
+            autoFocus
           >
-            <ToggleButton value="non-completed">
-              غير المنجز
-            </ToggleButton>
-            <ToggleButton value="completed">
-              المنجز
-            </ToggleButton>
-            <ToggleButton value="all">
-              الكل 
-            </ToggleButton>
-          </ToggleButtonGroup>
-          {/* Filter Buttons */}
-          {/* Start All Todos */}
-            {displayTodosType === "completed" && completedTodos.length === 0 ? <Alert severity="info" className='mb-3 justify-center flex-row-reverse gap-2'>لا يوجد مهام مضافة حتى الآن</Alert> : todosJsx}
-          {/* End All Todos */}
-          {/* Start Input + Add Button */}
-          <div className="inputsFeilds">
-            <Grid container spacing={2} direction={'row-reverse'} >
-              <Grid size={4}>
-                <TextField value={titleInput} onChange={(e)=>(setTitleInput(e.target.value))} id="outlined-basic" label="عنوان المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField}/>
+            تحديث
+          </Button>
+          <Button onClick={handleUpdateDialogClose} className="!text-[#b23c17]">
+            إغلاق
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* END UPDATE DIALOG */}
+      <Container maxWidth="md">
+        {todos.length === 0 ? <Card sx={{ minWidth: 275 , maxHeight:"650px" , height:"100%" , overflow:"auto"  }}>
+          <CardContent>
+            <Typography variant='h2' className='title text-center !mb-4' style={{ fontWeight:"400" }}>
+              مهامى اليومية 
+            </Typography>
+            <Divider/>
+            {/* Filter Buttons */}
+            <ToggleButtonGroup
+              exclusive          
+              value={displayTodosType}  
+              aria-label="text alignment"
+              className="!flex !justify-center items-center ltr my-5"
+              onChange={changeDisplayedType}
+              color='primary'
+            >
+              <ToggleButton value="non-completed">
+                غير المنجز
+              </ToggleButton>
+              <ToggleButton value="completed">
+                المنجز
+              </ToggleButton>
+              <ToggleButton value="all">
+                الكل 
+              </ToggleButton>
+            </ToggleButtonGroup>
+            {/* Filter Buttons */}
+            {/* Alert No assignments */}
+              <Alert severity="error" className='mb-3 justify-center flex-row-reverse gap-2'>لا يوجد مهام مضافة حتى الآن</Alert>
+            {/* Alert No assignments */}
+            {/* Start Input + Add Button */}
+            <div className="inputsFeilds">
+              <Grid container spacing={2} direction={'row-reverse'} >
+                <Grid size={4}>
+                  <TextField value={titleInput} onChange={(e)=>(setTitleInput(e.target.value))} id="outlined-basic" label="عنوان المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField} />
+                </Grid>
+                <Grid size={4}>
+                  <TextField value={detailsInput} onChange={(e)=>(setDetailsInput(e.target.value))} id="outlined-basic" label="تفاصيل المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField} />
+                </Grid>
+                <Grid size={4}>
+                  <Button onClick={()=>{handleAddClick()}} variant="contained" sx={{ width:"100%" , height:"100%" }} className={`${titleInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${detailsInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'}`}>إضافة</Button>
+                </Grid>
               </Grid>
-              <Grid size={4}>
-                <TextField value={detailsInput} onChange={(e)=>(setDetailsInput(e.target.value))} id="outlined-basic" label="تفاصيل المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField}/>
+            </div>
+            {/* End Input + Add Button */}
+          </CardContent>
+        </Card> : <Card sx={{ minWidth: 275 , maxHeight:"650px" , height:"100%" , overflow:"auto"  }}>
+          <CardContent>
+            <Typography variant='h2' className='text-center !mb-4' style={{ fontWeight:"400" }}>
+              مهامى اليومية
+            </Typography>
+            <Divider/>
+            {/* Filter Buttons */}
+            <ToggleButtonGroup
+              exclusive          
+              value={displayTodosType}  
+              aria-label="text alignment"
+              className="justify-center items-center ltr my-5"
+              onChange={changeDisplayedType}
+              color='primary'
+            >
+              <ToggleButton value="non-completed">
+                غير المنجز
+              </ToggleButton>
+              <ToggleButton value="completed">
+                المنجز
+              </ToggleButton>
+              <ToggleButton value="all">
+                الكل 
+              </ToggleButton>
+            </ToggleButtonGroup>
+            {/* Filter Buttons */}
+            {/* Start All Todos */}
+              {displayTodosType === "completed" && completedTodos.length === 0 ? <Alert severity="info" className='mb-3 justify-center flex-row-reverse gap-2'>لا يوجد مهام منجزة حتى الآن</Alert> : todosJsx}
+            {/* End All Todos */}
+            {/* Start Input + Add Button */}
+            <div className="inputsFeilds">
+              <Grid container spacing={2} direction={'row-reverse'} >
+                <Grid size={4}>
+                  <TextField value={titleInput} onChange={(e)=>(setTitleInput(e.target.value))} id="outlined-basic" label="عنوان المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField}/>
+                </Grid>
+                <Grid size={4}>
+                  <TextField value={detailsInput} onChange={(e)=>(setDetailsInput(e.target.value))} id="outlined-basic" label="تفاصيل المهمه" variant="outlined" className='w-[100%]' sx={rtlTextField}/>
+                </Grid>
+                <Grid size={4}>
+                  <Button onClick={()=>{handleAddClick()}} variant="contained" sx={{ width:"100%" , height:"100%" }} className={`${titleInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${detailsInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'}`}>إضافة</Button>
+                </Grid>
               </Grid>
-              <Grid size={4}>
-                <Button onClick={()=>{handleAddClick()}} variant="contained" sx={{ width:"100%" , height:"100%" }} className={`${titleInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${detailsInput.length === 0 ? 'opacity-30 pointer-events-none' : 'opacity-100'}`}>إضافة</Button>
-              </Grid>
-            </Grid>
-          </div>
-          {/* End Input + Add Button */}
-        </CardContent>
-        {/* <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions> */}
-      </Card>}
-      
-      {/* لو تم الضغط على زر منجز ومكنش فى مهام منجزة alert ظهور */}
-          
-    </Container>
+            </div>
+            {/* End Input + Add Button */}
+          </CardContent>
+          {/* <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions> */}
+        </Card>}
+        
+        {/* لو تم الضغط على زر منجز ومكنش فى مهام منجزة alert ظهور */}
+            
+      </Container>
+    </>
   );
 }
